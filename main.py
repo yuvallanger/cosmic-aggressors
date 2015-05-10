46faab700c2137310ce55e86efb493e5a2399afa
Yuval Langer <yuval.langer@gmail.com>
2015-05-10 22:45:07 +0300

    no clue what's going on in here.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 from __future__ import print_function
+from builtins import range
 from builtins import object
 from kivy.app import App
 #from kivy.vector import Vector
@@ -9,8 +10,28 @@ from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.properties import NumericProperty  #, ObjectProperty  #, ReferenceListProperty
 from kivy.uix.widget import Widget
-from kivy.metrics import Metrics
+from kivy.metrics import Metrics, sp
 from kivy.logger import Logger
+from kivy.uix.floatlayout import FloatLayout
+
+
+class Aggressors(Widget):
+    pass
+
+
+class Aggressor(Widget):
+    offset = NumericProperty('10sp')
+
+    def move(self):
+        new_x = self.x + self.offset
+
+        is_beyond_rim = (
+            new_x < self.parent.right + sp(50) or
+            new_x > self.parent.left - sp(50))
+        if is_beyond_rim:
+            self.y -= self.offset
+        else:
+            self.x = new_x
 
 
 class PlayerShip(Widget):
@@ -25,18 +46,27 @@ class PlayerShip(Widget):
         if self.right > self.parent.right:
             self.right = self.parent.right
 
+    def fire_missile(self):
+        if len(self.missiles.children) < params.max_missile_num:
+            player_missile = PlayerMissile()
+            player_missile.center_x = self.center_x
+            player_missile.y = self.top
+
+            self.missiles.add_widget(player_missile)
+
+
 
 class AlienShip(Widget):
     pass
 
 
 class PlayerMissile(Widget):
-    velocity = NumericProperty(2)
+    velocity = NumericProperty('2sp')
 
     def move(self):
         self.y += self.velocity
 
-        if self.y > self.parent.parent.height:
+        if self.y > self.ids.game.height:
             self.parent.remove_widget(self)
 
 
@@ -44,31 +74,38 @@ class PlayerMissiles(Widget):
     pass
 
 
-class AggressorsGame(Widget):
+class CosmicAggressorsGame(FloatLayout):
     def init_ship(self):
-        self.ids.player_ship.size = 20, 15
         self.ids.player_ship.center_x = self.center_x
-        self.ids.player_ship.y = 10
+        self.ids.player_ship.missiles = PlayerMissiles()
 
-    def fire_player_missile(self, *args):
-        if len(self.ids.player_missiles.children) < params.max_missile_num:
-            player_missile = PlayerMissile()
-            player_missile.center_x = self.ids.player_ship.center_x
-            player_missile.y = self.ids.player_ship.top
+    def init_aggressors(self):
+        for col in range(params.aggressors_col_num):
+            for row in range(params.aggressors_row_num):
+                relative_x = float(col)
+                relative_y = float(row)
 
-            self.ids.player_missiles.add_widget(player_missile)
+                aggressor = Aggressor()
+                aggressor.x = sp(50) + sp(50) * relative_x
+                aggressor.y = sp(150) + sp(50) * relative_y
 
-    def init_aliens(self):
-        pass
+                self.ids.aggressors.add_widget(aggressor)
+        Logger.debug("{}", self.ids.aggressors.children[:])
 
     def move_missiles(self):
-        for missile in self.ids.player_missiles.children[:]:
+        for missile in self.ids.player_ship.missiles.children[:]:
             missile.move()
+
+    def move_aggressors(self):
+        for aggressor in self.ids.aggressors.children[:]:
+            pass # aggressor.move()
+
 
     def update(self, dt):
         self.ids.player_ship.move()
         self.move_missiles()
-        Logger.debug("{}".format([(i, m.pos, m.size) for (i, m) in enumerate(self.ids.player_missiles.children)]))
+        self.move_aggressors()
+        #Logger.debug("{}".format([(i, m.pos, m.size) for (i, m) in enumerate(self.ids.player_ship.missiles.children)]))
 
     def on_size(self, *args):
         Logger.debug(
@@ -81,23 +118,29 @@ class AggressorsGame(Widget):
                 self=params,
             ))
 
-    def _on_keyboard_down(self, window, b, keycode, text, modifiers):
-        Logger.debug("{}".format(type(b)))
-        if text == u" ":
-            self.fire_player_missile()
-        Logger.debug("{}".format([window, b, keycode, text, modifiers]))
+    def _on_keyboard_down(self, window, key, scancode, unicode_key, modifiers):
+        Logger.debug("{}".format([window, key, scancode, unicode_key, modifiers]))
+        if unicode_key == u" ":
+            self.ids.player_ship.fire_missile()
+        if unicode_key == u"h":
+            self.ids.player_ship.move_left()
+        if unicode_key == u"l":
+            self.ids.player_ship.move_right()
+
+    def _on_keyboard_up(self, window, key, scancode, unicode_key, modifiers):
+        Logger.debug("{} {} {} {} {}".format(window, key, scancode, unicode_key, modifiers))
 
 
 class GameApp(App):
     def build(self):
         params.init()
 
-        game = AggressorsGame()
+        game = CosmicAggressorsGame()
 
-        game.size = 600, 400
+        game.root = game
 
         game.init_ship()
-        game.init_aliens()
+        game.init_aggressors()
 
         Window.bind(on_key_down=game._on_keyboard_down)
 
@@ -108,17 +151,19 @@ class GameApp(App):
 
 class Params(object):
     def init(self):
-        self.bg_width, self.bg_height = 600, 400
+        self.bg_size = self.bg_width, self.bg_height = 600, 400
         self.width, self.height = Window.size
         self.center = Window.center
         ws = float(self.width) / self.bg_width
         hs = float(self.height) / self.bg_height
         self.scale = min(ws, hs)
         self.max_missile_num = 3
+        self.aggressors_col_num = 0
+        self.aggressors_row_num = 0
         Logger.debug(
             "size={Window.size}; "
             "dpi={Metrics.dpi}; "
-            "density={Metrics.dpi}; "
+            "density={Metrics.density}; "
             "SCALE={self.scale}".format(
                 Window=Window,
                 Metrics=Metrics,
